76f611961395ab03f5256b8efff84676999a0a7c
henriqclaro <henrique.claro@unesp.br>
2026-05-09 12:13:30 -0300

    comitei

--- app/Http/Controllers/Admin/CursoController.php
+++ app/Http/Controllers/Admin/CursoController.php
@@ -14,4 +14,21 @@ class CursoController extends Controller
 
         return view('admin.cursos.index', compact('rows'));
     }
+
+    public function adicionar()
+    {
+        return view('admin.cursos.adicionar');
+    }
+
+    public function editar($id)
+    {
+        $linha = Curso::find($id);
+        return view('admin.cursos.editar', compact('linha'));
+    }
+
+    public function excluir($id)
+    {
+        Curso::find($id)->delete();
+        return redirect()->route('admin.cursos');
+    }
 }
